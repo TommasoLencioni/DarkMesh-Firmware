8f47c7fe19bfb267918c2e5396243a3b671e0218
unknown <you@example.com>
2026-02-27 13:47:35 +0100

    fix build for windows for tlora pager and add 5sec traceroute cd

--- src/modules/TraceRouteModule.h
+++ src/modules/TraceRouteModule.h
@@ -75,7 +75,7 @@ class TraceRouteModule : public ProtobufModule<meshtastic_RouteDiscovery>,
     TraceRouteRunState runState = TRACEROUTE_STATE_IDLE;
     unsigned long lastTraceRouteTime = 0;
     unsigned long resultShowTime = 0;
-    unsigned long cooldownMs = 30000;
+    unsigned long cooldownMs = 5000;
     unsigned long resultDisplayMs = 10000;
     unsigned long trackingTimeoutMs = 10000;
     String bannerText;
